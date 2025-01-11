e4b841d74daf6533e6d8d353f4cc96f1d2e5a077
Vladislav Kraemer <v21kraemer@gmail.com>
2025-01-11 05:50:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+let randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+let randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+let randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
 
-var image1 = document.querySelectorAll("img")[0];
+let image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
 
